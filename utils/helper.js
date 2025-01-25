eec251185d3acfde15ed7081991fd34e796f1e12
Syaam Khandaker <syaamkhandaker@gmail.com>
2025-01-24 20:45:10 -0500

    updated helper file to retrieve more files

--- utils/helper.js
+++ utils/helper.js
@@ -1,16 +1,18 @@
 import * as cheerio from 'cheerio';
 
-export const findChildLinks = (body) => {
+export const findChildLinks = (url, $) => {
     const links = [];
 
     // jquery to find links with valid href tags
-    const $ = cheerio.load(body);
     $('a').filter((i, el) => {
         const href = $(el).attr('href');
-        if (href && href.includes('http') && href.includes('cc.gatech.edu')) {
-            if (href.startsWith('/')) {
-                links.push(`https://cc.gatech.edu${href}`)
-            }
+        console.log(href)
+        if (href && href.startsWith('/')) {
+            const newLink = new URL(href, url).href
+            console.log(newLink)
+            links.push(newLink)
+        }
+        else if (href && href.includes('http') && href.includes('cc.gatech.edu')) {
             links.push(href);
         }
     });
