4d34dbcd3ade9b8e2cea4be40855650cee880350
Syaam Khandaker <70422626+syaamkhandaker@users.noreply.github.com>
2025-01-25 05:46:07 -0500

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -1,4 +1,6 @@
 import { BASE_DOMAIN } from './constants.js';
+import fs from 'fs';
+import plot from 'nodeplotlib'
 
 export const findChildLinks = (url, $) => {
     const links = [];
@@ -7,8 +9,8 @@ export const findChildLinks = (url, $) => {
     $('a').filter((i, el) => {
         const href = $(el).attr('href');
         if (href && href.startsWith('/')) {
-            const newLink = new URL(href, url).href
-            links.push(newLink)
+            const newLink = new URL(href, url).href;
+            links.push(newLink);
         }
         else if (href && href.includes('http') && href.includes(BASE_DOMAIN)) {
             links.push(href);
@@ -16,4 +18,10 @@ export const findChildLinks = (url, $) => {
     });
     
     return links;
+}
+
+export const plotStats = () => {
+    plot.plot([JSON.parse(fs.readFileSync('keyword.json'))], {'xaxis.title': 'Time (ms)', 'yaxis.title': "Number of Keywords"})
+    plot.plot([JSON.parse(fs.readFileSync('extracted_links.json'))], {'xaxis.title': 'Time (ms)', 'yaxis.title': "Number of Extracted Links"})
+    plot.plot([JSON.parse(fs.readFileSync('crawled_links.json'))], {'xaxis.title': 'Time (ms)', 'yaxis.title': "Number of Crawled Links"})
 }
